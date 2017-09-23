d583fb67da837d6bb7b7b7a2072595a9bc2b07ab
minhyung-joo <mjoo@connect.ust.hk>
2017-09-23 09:33:38 +0800

    port setup

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const apiRoutes = require('./controllers/routes')
 const app = express()
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', apiRoutes)
 
-app.listen(3000)
-console.log('Listening on port 3000')
+app.listen(port)
+console.log(`Listening on port ${port}`)
