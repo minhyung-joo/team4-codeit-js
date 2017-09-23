98ce8682754d6156a3a9e796361bda6005b6e5f5
minhyung-joo <mjoo@connect.ust.hk>
2017-09-23 11:48:39 +0800

    set body limit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./controllers/routes')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', apiRoutes)
